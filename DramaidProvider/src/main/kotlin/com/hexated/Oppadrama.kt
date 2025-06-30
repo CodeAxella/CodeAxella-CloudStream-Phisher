c82193b47188069a32efa7dfe6c1e420097704fc
Axella <codeaxella@gmail.com>
2025-06-30 14:04:12 +0700

    Create Oppadrama.kt

---
+++ DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -0,0 +1,8 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.extractors.Filesim
+
+class Oppadrama : DramaidProvider() {
+    override var mainUrl = "http://45.11.57.248/"
+    override var name = "Oppadrama"
+}
